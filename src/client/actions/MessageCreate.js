3d91d40f868d54f876682b23539a2c3c617f36aa
Kalissaac <kalissaac@protonmail.com>
2022-06-13 09:33:37 -0700

    fix: properly fix error

--- src/client/actions/MessageCreate.js
+++ src/client/actions/MessageCreate.js
@@ -6,8 +6,9 @@ const { Events } = require('../../util/Constants');
 class MessageCreateAction extends Action {
   handle(data) {
     const client = this.client;
-    const channel = client.channels && client.channels.cache.get(data.channel_id);
+    const channel = client.channels.cache.get(data.channel_id);
     if (channel) {
+      if (!channel.messages) return {};
       const existing = channel.messages.cache.get(data.id);
       if (existing) return { message: existing };
       const message = channel.messages.add(data);
